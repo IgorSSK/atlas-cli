fa14295f3f3c59b580f9400b429c0453cbb42d8a
Igor Souza <igors@DESKTOP-MO5U0SD>
2022-12-22 11:21:54 -0300

    fix: comments

--- servers/valheim/server.sh
+++ servers/valheim/server.sh
@@ -2,41 +2,41 @@
 
 echo current user $USER
 
-echo ---------------------------------------------------------
+echo "---------------------------------------------------------"
 
 apt update
 
-echo ---------------------------------------------------------
+echo "---------------------------------------------------------"
 
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 apt install docker-compose
 
-echo ---------------------------------------------------------
+echo "---------------------------------------------------------"
 
 # Install GIT
 apt install git
 git --version
 
-echo ---------------------------------------------------------
+echo "---------------------------------------------------------"
 
 # Install Cron
 apt-get install cron
 
-echo ---------------------------------------------------------
+echo "---------------------------------------------------------"
 
 # Download saves
 mkdir -p $HOME/valheim-server/config/worlds_local $HOME/valheim-server/data
 wget -P $HOME/valheim-server/config/worlds_local https://github.com/IgorSSK/GameCenter.DServers/blob/main/servers/valheim/saves/Gameplay.db
 wget -P $HOME/valheim-server/config/worlds_local https://github.com/IgorSSK/GameCenter.DServers/blob/main/servers/valheim/saves/Gameplay.fwl
 
-echo ---------------------------------------------------------
+echo "---------------------------------------------------------"
 
 # Get Docker file from GIT
 curl https://github.com/IgorSSK/GameCenter.DServers/blob/main/servers/valheim/docker-compose.yml
 
-echo ---------------------------------------------------------
+echo "---------------------------------------------------------"
 
 # Up Container
 docker-compose up
