653ea03cc9dac16bf3e1c268ef6f748bc24c7cf2
Igor Souza <igors@DESKTOP-MO5U0SD>
2022-12-22 11:10:27 -0300

    automating server setup

--- servers/valheim/server.sh
+++ servers/valheim/server.sh
@@ -2,18 +2,39 @@
 
 echo current user $USER
 
+apt update
+
+echo \n---------------------------------------------\n
+
 #Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 apt install docker-compose
 
+echo \n---------------------------------------------\n
+
+#Install GIT
+apt install git
+git --version
+
+echo \n---------------------------------------------\n
+
+#Install Cron
+apt-get install cron
+
+echo \n---------------------------------------------\n
+
 #Download saves
 mkdir -p $HOME/valheim-server/config/worlds_local $HOME/valheim-server/data
-wget -P $HOME/valheim-server/config/worlds_local https://releases.ubuntu.com/20.04/ubuntu-20.04-desktop-amd64.iso
-wget -P $HOME/valheim-server/config/worlds_local https://releases.ubuntu.com/20.04/ubuntu-20.04-desktop-amd64.iso
+wget -P $HOME/valheim-server/config/worlds_local https://github.com/IgorSSK/GameCenter.DServers/blob/main/servers/valheim/saves/Gameplay.db
+wget -P $HOME/valheim-server/config/worlds_local https://github.com/IgorSSK/GameCenter.DServers/blob/main/servers/valheim/saves/Gameplay.fwl
+
+echo \n---------------------------------------------\n
 
 #Get Docker file from GIT
 curl https://gist.githubusercontent.com/robzhu/a127a6bce1ea25b01d40efb57ad1c26e/raw/30a2927a901dd614a518319cfeaa63a6bd2648a4/gistfile1.txt
+
+echo \n---------------------------------------------\n
 
 #Up Container
 docker-compose up
